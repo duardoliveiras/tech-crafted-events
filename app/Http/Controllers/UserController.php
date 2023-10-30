7e29ce41ac619c5f296371423c8d95efd7bb664f
brito-bernardo <bernardo_brito1@hotmail.com>
2023-10-30 19:18:21 +0000

    Realizando a criação e correção de erros das views de edit/update/destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -57,9 +57,21 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            // In case of profile not found
+            abort(404);
+        }
+
+        if (Auth::user()->id !== $user->id) {
+            // Prevent users from editing other users' profiles
+            return redirect()->route('home')->with('error', 'You do not have permission to edit.');
+        }
+
+        return view('layouts.profile.edit')->with('user', $user);
     }
 
     /**
@@ -67,14 +79,55 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            // Profile not found
+            abort(404);
+        }
+
+        if (Auth::user()->id !== $user->id) {
+            // Prevent users from updating other users' profiles
+            return redirect()->route('home')->with('error', 'You do not have permission to edit');
+        }
+
+        // Data validation
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'birthdate' => 'required|date',
+            // Note: The unique validation rule should reference the correct table
+            'email' => 'required|email|max:255|unique:users,email,'.$id,
+            'phone_number' => 'required|max:20'
+        ]);
+
+        // Update profile
+        $user->update($validatedData);
+
+        // Redirect to the profile show
+        // Note: Make sure the route name is correct. It should not include the view path.
+        return redirect()->route('profile.show', $user->id)->with('success', 'Profile updated sucessfully!');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            abort(404);
+        }
+
+        // User deleted
+        $user->isDeleted = true;
+        $user->save();
+
+        //Logout
+        Auth::logout();
+
+        return redirect()->route('login')->with('success', 'Account has been deleted.');
     }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -29,8 +29,8 @@ class User extends Authenticatable
         'email',
         'password',
         'phone',
-        'isdeleted',
-        'isbanned',
+        'isDeleted',
+        'isBanned',
 
     ];
 
