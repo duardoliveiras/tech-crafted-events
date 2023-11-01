797f97c213cff5a694d4568b58d7b5794e68db15
brito-bernardo <bernardo_brito1@hotmail.com>
2023-11-01 22:35:18 +0000

    Create event_table

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,7 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Category;
+
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'startdate',
+        'enddate',
+        'startticketsqty',
+        'currentticketsqty',
+        'currentprice',
+        'address',
+        'category_id',
+        'city_id',
+        'owner_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
