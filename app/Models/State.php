2d968152740d3aca7abcf0c51569475ccfcf50c4
Vicente-MD <vicentemeidam@gmail.com>
2023-11-03 02:33:47 +0000

    added filter options and footer

--- app/Models/State.php
+++ app/Models/State.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    use HasFactory;
+    protected $table = 'state';
+
+    protected $fillable = ['name', 'initials', 'country_id'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
 }
