9b1c1329fefd3b737b9c4fb31c5ceba3596907e2
duardoliveiras <duardooliveiras@gmail.com>
2023-12-09 20:05:35 +0000

    implement admin report

--- resources/views/layouts/admin/reports.blade.php
+++ resources/views/layouts/admin/reports.blade.php
@@ -3,6 +3,84 @@
 
 @section('content')
 
-    Reports
+    <div class="container mt-4">
+        <h2 class="mb-4">Reports</h2>
+            
+            <div class="row">
+                <div class="col-md-6">
+                 <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">Event Name</th>
+                        <th scope="col">Reports</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
 
+                        @foreach ($events as $event )
+                            <td> {{ $event->name }} </td>
+                            <td> {{ $event->event_report_count }} </td>
+                        @endforeach
+                        <td>
+                        <button type="button" class="btn btn-primary">    
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#eye-fill') }}"></use></svg>
+                            View
+                        </button>
+
+                        <button type="button" class="btn btn-success">
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#check-square-fill') }}"></use></svg>
+                            Check
+                        </button>
+
+                        <button type="button" class="btn btn-danger">
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#ban-fill') }}"></use></svg>
+                            Ban
+                        </button>   
+                        </td>
+                    </tr>
+                    </tbody>
+                 </table>
+                </div>
+                         <div class="col-md-6">
+                 <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">Comment</th>
+                        <th scope="col">Reports</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Mulher tem mesmo é que lavar louça</td>
+                        <td>5.000</td>
+                        <td>
+                        <button type="button" class="btn btn-primary">    
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#eye-fill') }}"></use></svg>
+                            View
+                        </button>
+
+                        <button type="button" class="btn btn-success">
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#check-square-fill') }}"></use></svg>
+                            Check
+                        </button>
+
+                        <button type="button" class="btn btn-danger">
+                            <svg class="bi" width="16" height="16"><use xlink:href="{{ asset('assets/svg/icons.svg#ban-fill') }}"></use></svg>
+                            Ban
+                        </button>                       
+                        </td>
+                    </tr>
+                    </tbody>
+                 </table>
+                </div>
+            </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 @endsection
