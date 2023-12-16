ac305d76cdd193cc01396a289dd90b88b407784a
Vicente Damasceno <vicentemdamasceno@gmail.com>
2023-12-16 01:13:54 +0000

    Sorting works in events list

--- public/js/event/list-event.js
+++ public/js/event/list-event.js
@@ -8,3 +8,15 @@ function clearForm() {
 
     window.location.href = currentUrl.href;
 }
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function() {
+        // Set the sort option in a hidden form field
+        document.getElementById('sort').value = this.getAttribute('data-sort');
+
+        console.log(this.getAttribute('data-sort'));
+
+        // Submit the form
+        document.getElementById('filter-form').submit();
+    });
+});
