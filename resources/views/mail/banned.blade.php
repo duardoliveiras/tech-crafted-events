db564c2255be558a821aa8c3a9249d6a743ec84c
duardoliveiras <duardooliveiras@gmail.com>
2023-12-18 19:48:07 +0000

    send email for user banned

---
+++ resources/views/mail/banned.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Banned</title>
+</head>
+<body style="font-family: 'Arial', sans-serif;">
+
+    <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f8f8f8; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <h2 style="color: #e44d26;">Account Banned</h2>
+
+        <p>We regret to inform you that your account has been banned due to a violation of our platform rules.</p>
+
+        <p>If you believe this ban is in error or would like to appeal, please contact our support team at support@example.com.</p>
+
+        <p>Thank you for your understanding.</p>
+
+        <p>Best regards,<br>Your Platform Team</p>
+    </div>
+
+</body>
+</html>
