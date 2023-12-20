05b576d9f969cb8b95e9e694dc094ac3e5936fce
duardoliveiras <duardooliveiras@gmail.com>
2023-12-20 15:23:22 +0000

    adjust sql

--- routes/web.php
+++ routes/web.php
@@ -112,12 +112,14 @@ Route::resource('universities', UniversityController::class);
 //Ticket
 Route::get('/events/{event}/ticket/buy', [TicketController::class, 'showBuyTicketForm'])->name('ticket.buy');
 Route::post('/events/{event}/ticket/acquire', [TicketController::class, 'acquireTicket'])->name('ticket.acquire');
+Route::post('/events/{event}/ticket/invite', [TicketController::class, 'acquireInvite'])->name('ticket.invite');
 Route::get('/events/{event}/ticket/authorize', [TicketController::class, 'authorizeTicket'])->name('ticket.authorize');
 Route::get('/events/{event}/ticket/{ticket}', [TicketController::class, 'showTicket'])
     ->name('ticket.show')
     ->middleware(['auth', 'acess.ticket']);
 Route::post('/events/{event}/ticket/authenticate', [TicketController::class, 'authenticateTicket'])->name('ticket.authenticate');
 Route::get('/download-ticket/{eventId}/{ticketId}', [TicketController::class, 'downloadTicket'])->name('ticket.download');
+
 
 
 
