499e2833f5554e05ca1d10688434ce6ac1184cff
duardoliveiras <duardooliveiras@gmail.com>
2023-12-21 20:21:29 +0000

    adjust breadcrumps

--- resources/views/layouts/my_events/index.blade.php
+++ resources/views/layouts/my_events/index.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'My Events')
 
 @section('content')
+@section('breadcrumbs')
+<li>
+    &nbsp; / My Events
+</li>
+@endsection
     <div class="container py-5">
         <h1 class="text-center mb-5">My Events</h1>
 
